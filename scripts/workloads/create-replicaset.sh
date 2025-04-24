bbf8bd22372c715bfbab3dc728eb16152d222b7c
infralliance <infralliance.io@gmail.com>
2025-04-24 15:10:15 +0100

    remove replicaset script

--- scripts/workloads/create-replicaset.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Auto-patched for House of Kube
-# Usage: ./create-replicaset.sh <name> [namespace]
-
-[ -z "$1" ] && echo "Usage: ./create-replicaset.sh <name> [namespace]" && exit 1
-NAME="$1"
-NS="${2:-default}"
-
-kubectl create deployment "$NAME" --image=nginx --replicas=3 --dry-run=client -n "$NS" -o yaml | kubectl apply -f -
